dc48dd538380524182ec36f31474ecdb9b7b731f
Kauã Maximino <kaua_maximino@outlook.com>
2022-04-01 16:01:35 -0300

    requisito 2 ok

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -17,9 +17,7 @@ class Login extends React.Component {
   checkedForm = () => {
     const { email, password } = this.state;
     const six = 6;
-    const disabled = ((password.length >= six) && (/\S+@\S+\.\S+/.test(email)));
-    // console.log(disabled);
-    if (disabled) {
+    if ((password.length >= six) && (/\S+@\S+\.\S+/.test(email))) {
       return false;
     }
     return true;
@@ -76,81 +74,3 @@ Login.propTypes = {
 }.isRequired;
 
 export default connect(null, mapDispatchToProps)(Login);
-
-// import React from 'react';
-// // import { connect } from 'react-redux';
-// import { Link } from 'react-router-dom';
-// import PropTypes from 'prop-types';
-// // import { userAction } from '../actions';
-
-// class Login extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       email: '',
-//       password: '',
-//     };
-//   }
-
-// validation = () => {
-//   const { email, password } = this.state;
-//   const regex = /\S+@\S+\.\S+/; // regex https://www.horadecodar.com.br/2020/09/07/expressao-regular-para-validar-e-mail-javascript-regex/
-//   const sixValidation = 6;
-//   if ((password.length >= sixValidation) && (regex.test(email))) {
-//     return false;
-//   }
-//   return true;
-// }
-
-// handleChange = (event) => {
-//   const { name, value } = event.target;
-//   this.setState({ [name]: value });
-// // this.validation();
-// }
-
-// render() {
-//   const { email, password } = this.state;
-//   const { user } = this.props;
-//   return (
-//     <div>
-//       <h1>Login</h1>
-//       <input
-//         type="text"
-//         onChange={ this.handleChange }
-//         placeholder="E-mail"
-//         data-testid="email-input"
-//         value={ email }
-//         name="email"
-//       />
-//       <input
-//         type="password"
-//         onChange={ this.handleChange }
-//         placeholder="Senha"
-//         data-testid="password-input"
-//         value={ password }
-//         name="password"
-//       />
-//       <Link to="/carteira">
-//         <button
-//           type="submit"
-//           disabled={ this.validation() }
-//           onClick={ () => user({ email }) }
-//         >
-//           Entrar
-
-//         </button>
-//       </Link>
-//     </div>);
-// }
-// }
-
-// // const mapDispatchToProps = (dispatch) => ({
-// //   user: (elem) => dispatch(emailUser(elem)),
-// // });
-
-// Login.propTypes = {
-//   user: PropTypes.object,
-// }.isRequired;
-
-// export default Login;
-// // connect(null, mapDispatchToProps)(Login);
